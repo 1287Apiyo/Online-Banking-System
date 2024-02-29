7ecc5a03f1c611a1f8d185fa0eaf7c65035be246
Anne Apiyo <anneapiyo0@gmail.com>
2024-02-29 16:25:17 +0300

    Add files via upload

---
+++ Transaction.java
@@ -0,0 +1,8 @@
+import java.io.Serializable;
+
+public class Transaction implements Comparable<Transaction>, Serializable {
+    @Override
+    public int compareTo(Transaction o) {
+        return 0;
+    }
+}
