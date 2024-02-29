628312329736131cec9c64e0def97cafa701297c
Anne Apiyo <anneapiyo0@gmail.com>
2024-02-29 16:12:26 +0300

    Add files via upload

--- Accounts.java
+++ Accounts.java
@@ -14,11 +14,13 @@ public class Accounts implements Comparable{
 
 
     //let us now create the constructor
-    public Account(){
+    public Accounts(){
 
     }
 
-    public Account(String owner, City city, char gender) {
+
+//constructors
+    public Accounts(String owner, City city, char gender) {
         accNo=nextAccNO;
         nextAccNO +=10;
         this.owner = owner;
@@ -26,10 +28,29 @@ public class Accounts implements Comparable{
         this.gender = gender;
         balance=0.0;
         openDate=null;  //System.currentDate();
+
+
     }
 
 
+    public Accounts(int accNo, String owner, City city, char gender, double balance) {
+        this.accNo = accNo;
+        this.owner = owner;
+        this.city = city;
+        this.gender = gender;
+        setBalance(balance);
+    }
+    //setters
+    public void setBalance(double b) {
+        balance = b> 0.0 ? b: 0.0;
+    }
+//toString()
+    @Override
+    public String toString() {
+        return accNo + " "+  owner +" " +city.cityName + " "
+              + gender + " " + balance;
 
+    }
 
     @Override
     public int compareTo(Object o) {
